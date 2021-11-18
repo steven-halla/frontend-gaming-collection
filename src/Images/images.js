0f30b441fa14b4f86b93fd4f711932216d597a16
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-11-18 13:27:19 -0800

    deleted images folder

--- src/Images/images.js
+++
@@ -1,5 +0,0 @@
-// nintendo stacked tapes:
-// https://i.pinimg.com/originals/0b/78/5a/0b785a4b2ff7d0e1c22dc1d59c30eb91.jpg
-
-// nintendo tapes 2 image
-// https://images.shopgoodwill.com/11/9-6-2019/45960636121040Mpna.jpg
