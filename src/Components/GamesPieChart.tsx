741735fd3411903ffe7fe555e7896dd3e38e2b87
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-11 18:36:16 -0800

    "converting gamepie chart to tsx"

--- src/Components/GamesPieChart.tsx
+++ src/Components/GamesPieChart.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {Pie, PieChart, Tooltip} from 'recharts';
 
-export const GamesPieChart = (props) => {
+interface GamesPieChartProps {
+  data: any;
+  width: number;
+  height: number;
+}
+
+interface renderCustomizedLableInterface {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  name: string;
+  value: number;
+}
+
+export const GamesPieChart: FC = (props: GamesPieChartProps) => {
   const {data, width = 400, height = 400} = props;
   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
   const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index, name, value}) => {
+  const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, name, value}) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
